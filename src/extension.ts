e678266c1153416e187fc10362b5e30a70db67da
Yannis300307 <yannis300307@gmail.com>
2023-09-01 14:59:17 +0200

    fixed warnings

--- src/extension.ts
+++ src/extension.ts
@@ -16,59 +16,59 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	OS_NAME = get_os();
-	IS_WSL_INSTALLED = get_wsl_installed()
+	IS_WSL_INSTALLED = get_wsl_installed();
 
 	
-	if (OS_NAME == "unknown") {
-		log_warn('Casio Dev Tools extension is not actually compatible with your OS ... many features will not work.');
-	} else if (OS_NAME == "windows" && !IS_WSL_INSTALLED) {
-		log_warn('You need a WSL distribution to run fxsdk. Please install wsl to be able to compile.');
+	if (OS_NAME === "unknown") {
+		logWarn('Casio Dev Tools extension is not actually compatible with your OS ... many features will not work.');
+	} else if (OS_NAME === "windows" && !IS_WSL_INSTALLED) {
+		logWarn('You need a WSL distribution to run fxsdk. Please install wsl to be able to compile.');
 	}
 
-	IS_GITEAPC_INSTALLED = get_giteapc_installed()
+	IS_GITEAPC_INSTALLED = get_giteapc_installed();
 	
-	console.log("OS name ? " + OS_NAME)
-	console.log("Is wsl installed ? " + IS_WSL_INSTALLED)
-	console.log("Is GiteaPC installed ? " + IS_GITEAPC_INSTALLED)
+	console.log("OS name ? " + OS_NAME);
+	console.log("Is wsl installed ? " + IS_WSL_INSTALLED);
+	console.log("Is GiteaPC installed ? " + IS_GITEAPC_INSTALLED);
 
-	if (!IS_GITEAPC_INSTALLED || true) {
+	if (!IS_GITEAPC_INSTALLED) {
 		vscode.window
-		.showInformationMessage("GiteaPC is not installed on your system. Do you want to install it automaticaly?", "Yes", "No")
-		.then(answer=>{ask_password(answer)})
+			.showInformationMessage("GiteaPC is not installed on your system. Do you want to install it automaticaly?", "Yes", "No")
+			.then(answer => { askPassword(answer); });
 	}
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
-function log_warn(text: string) {
+function logWarn(text: string) {
 	vscode.window.showWarningMessage(text);
 		console.log(text);
 }
 
-function ask_password(answer:string | undefined, retry=false) {
+function askPassword(answer:string | undefined, retry=false) {
 	if (answer === "Yes") {
 		if (retry) {
-			var input_promt = "Bad password please retype it :";
+			var inputPromt = "Bad password please retype it :";
 		} else {
-			var input_promt = "Please type your linux/WSL superuser password :";
+			var inputPromt = "Please type your linux/WSL superuser password :";
 		}
 		const options:InputBoxOptions = {
 			password: true,
-			prompt: input_promt,
+			prompt: inputPromt,
 		};
 		vscode.window.showInputBox(options).then(async (value) => {
-			if (value == undefined) {
-				var password = ""
+			if (value === undefined) {
+				var password = "";
 			} else {
-				var password = value
+				var password = value;
 			}
 			var result = await install_giteapc(password);
-			ask_password("yes", true);
-			if (result[0] == "failed") {
-				log_warn('An error ocurred during the installation of GiteaPC : ' + result[1]);
-				if (result[2]) ask_password("yes", true);
-			} else if (result == "success") {
+			askPassword("yes", true);
+			if (result[0] === "failed") {
+				logWarn('An error ocurred during the installation of GiteaPC : ' + result[1]);
+				if (result[2]) {askPassword("yes", true);}
+			} else if (result === "success") {
 				vscode.window.showInformationMessage("GiteaPC is now ready to use on your system !");
 				IS_GITEAPC_INSTALLED = true;
 			}
