205386226803043ec895e7f2475709f5a2e6fa88
yannis300307 <yannis300307@gmail.com>
2023-09-04 12:55:09 +0200

    Improved webview css and moved html file

--- src/gitepc_webview.ts
+++ src/gitepc_webview.ts
@@ -58,7 +58,7 @@ export class GiteaPCViewProvider implements vscode.WebviewViewProvider {
     private _getHtmlForWebview(webview: vscode.Webview) {
 		var defaultHtml;
 		try {
-			defaultHtml = readFileSync(vscode.Uri.joinPath(this._extensionUri, "webviews", "giteapc", "index.html").fsPath, 'utf8');
+			defaultHtml = readFileSync(vscode.Uri.joinPath(this._extensionUri, "webviews", "giteapc.html").fsPath, 'utf8');
 		} catch (error) {
 			console.log(error);
 			defaultHtml = `<html><p>An error occured during the loading of the webview. Please reinstall CasioDevTools.</p></html>`;
