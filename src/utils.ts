05a18a0bd33f4267d206e67303c3c73d3f7e3fb4
Yannis300307 <yannis300307@gmail.com>
2023-09-28 18:37:28 +0200

    Added a generic loading with progress implementation

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,10 @@
 import * as vscode from 'vscode';
 
+var isLoading = false;
+var lastLog = "";
+var loadingsOptions: {[index: string]:{[index: string]: boolean | string}} = {};
+
+
 export function logWarn(text: string) {
 	vscode.window.showWarningMessage(text);
 	console.log(text);
@@ -8,4 +13,35 @@ export function logWarn(text: string) {
 export function logMessage(text: string) {
 	vscode.window.showInformationMessage(text);
 	console.log(text);
+}
+
+export function logLongLoading(title: string, id:string) {
+	isLoading = true;
+	loadingsOptions[id]["isLoading"] = true;
+	loadingsOptions[id]["lastLog"] = "";
+	vscode.window.withProgress({
+		location: vscode.ProgressLocation.Notification,
+		cancellable: false,
+		title: title
+	}, async (progress) => {
+		await updateProgress(progress, id);
+	});
+}
+
+export function setLoadingState(id:string, state: boolean) {
+	loadingsOptions[id]["isLoading"] = state;
+}
+
+export function setLastLog(id:string, log: string) {
+	loadingsOptions[id]["lastLog"] = log;
+}
+
+async function updateProgress(progress: vscode.Progress<{ message?: string | undefined; increment?: number | undefined; }>, id: string) {
+	if (!loadingsOptions[id]["isLoading"]) { return; }
+	const poll = (resolve: any) => {
+		if (!loadingsOptions[id]["isLoading"]) { resolve(); }
+		else { setTimeout((_: any) => { poll(resolve); progress.report({ message: (loadingsOptions[id]["lastLog"] as string) }); }, 100); }
+	};
+
+	return new Promise(poll);
 }
